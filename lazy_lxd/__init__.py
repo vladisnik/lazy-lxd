b511113e780355f205266f7c7acd095c9a3a825b
Vladislav Nikitin <gomilt@gmail.com>
2020-08-09 02:51:01 +0300

    Incrase version to 1.0.1.
    For uploading package with fixed metadata.

--- lazy_lxd/__init__.py
+++ lazy_lxd/__init__.py
@@ -5,6 +5,6 @@ This module contains the main logic to create container
 and running ansible playbooks over it.
 """
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Vladislav Nikitin'
 __license__ = 'MIT'
